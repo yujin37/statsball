c10b9e532a3a705f5c7f1ce580b87947eec604d7
yujin37 <yj061628@gmail.com>
2025-11-09 19:33:10 +0900

    chore(backend): Spring Boot 백엔드 프로젝트 초기화

---
+++ backend/statsball-service/statsball-service/src/test/java/com/statsball/statsball_service/StatsballServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statsball.statsball_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsballServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
