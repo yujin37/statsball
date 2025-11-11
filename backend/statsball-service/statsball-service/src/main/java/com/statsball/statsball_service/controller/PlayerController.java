7c9ec08dcb9860f4eaa61769f201565bccd95437
yujin37 <yj061628@gmail.com>
2025-11-11 20:12:40 +0900

    feat(backend): 전체 타자 조회 서비스 객체로 분리

--- backend/statsball-service/statsball-service/src/main/java/com/statsball/statsball_service/controller/PlayerController.java
+++ backend/statsball-service/statsball-service/src/main/java/com/statsball/statsball_service/controller/PlayerController.java
@@ -2,6 +2,7 @@ package com.statsball.statsball_service.controller;
 
 import com.statsball.statsball_service.domain.Player;
 import com.statsball.statsball_service.repository.PlayerRepository;
+import com.statsball.statsball_service.service.PlayerService;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/player")
 public class PlayerController {
-    private final PlayerRepository playerRepository;
+    private final PlayerService playerService;
 
-    public PlayerController(PlayerRepository playerRepository){
-        this.playerRepository = playerRepository;
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
     }
 
     @GetMapping
     public List<Player> allPlayers() {
-        return playerRepository.findAll();
+        return playerService.getAllPlayers();
     }
 }
