637fc32c1870fb598665e40e332cf0db39eda581
yujin37 <yj061628@gmail.com>
2025-11-20 16:36:55 +0900

    feat(frontend): 선수 기록 페이지 내 탭 추가

--- frontend/statsball/src/pages/player-stats/pitchers.tsx
+++ frontend/statsball/src/pages/player-stats/pitchers.tsx
@@ -1,9 +1,117 @@
+import { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout"
+import style from "./pitchers.module.scss"
+import axios from "axios";
+
+interface PitcherTab {
+  tabName: string;
+  rule: string;
+}
+
+const pitcherTabList: PitcherTab[] = [
+        {tabName:'전체', rule: "전체"},
+        {tabName:'선발 투수', rule: '선발투수'},
+        {tabName:'불펜 투수', rule: '불펜투수'}, 
+        {tabName:'마무리 투수', rule: '마무리투수'},
+        {tabName:'그외', rule: '그외'}
+];
+
+interface Pitcher {
+    id: number;
+    teams: string;
+    name: string;
+    era: number;
+    game: number;
+    wins: number;
+    losses: number;
+    saves: number;
+    holds: number 
+    inning: string;
+    whip: number;
+    qs: number;
+}
 
 const PitchStats = () => {
-    return (
+    const  [tab, setTab] = useState(0)
+    const [pitchers, setPitchers] = useState<Pitcher[]>([]);
+    const [loading, setLoading] = useState(false);
+    const selectTabPitcher = (index: number) => {
+        setTab(index);
+    }
+
+    const rule = pitcherTabList[tab].rule;
+
+    const setUrl = "http://168.107.37.97:8080/api/pitcher/rolePitchers?rule="
+    const fetchPitchers = async (rule: string) => {
+        try {
+            setLoading(true);
+            const res = await axios.get<Pitcher[]>(
+                setUrl + encodeURIComponent(rule)
+            );
+        setPitchers(res.data);
+        } catch(error) {
+            console.error('데이터 가져오기 오류', error);
+            return []
+        }
+    } 
+
+    useEffect(() => {
+        fetchPitchers(rule);
+        }, [rule]);
+    
+        return (
         <Layout>
         <h2>투수 기록</h2>
+
+        <div className="tabContainer">
+            <ul>
+                {pitcherTabList.map((x, index) => (
+                    <li
+                    key = {index}
+                    onClick={() => selectTabPitcher(index)}
+                    className={tab === index ? "on": ""}
+                    >
+                        {x.tabName}
+                    </li>
+                ))}
+            </ul>
+        </div>
+        <div className="tabContent">
+            {pitchers.map(p => (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>팀</th>
+                            <th>이름</th>
+                            <th>ERA</th>
+                            <th>출장 게임 수</th>
+                            <th>승</th>
+                            <th>패</th>
+                            <th>세이브</th>
+                            <th>홀드</th>
+                            <th>이닝</th>
+                            <th>WHIP</th>
+                            <th>QS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr key = {p.id}>
+                            <td>{p.teams}</td>
+                            <td>{p.name}</td>
+                            <td>{p.era}</td>
+                            <td>{p.game}</td>
+                            <td>{p.wins}</td>
+                            <td>{p.losses}</td>
+                            <td>{p.saves}</td>
+                            <td>{p.holds}</td>
+                            <td>{p.inning}</td>
+                            <td>{p.whip}</td>
+                            <td>{p.qs}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            ))}
+        </div>
         </Layout>
     )
 }
