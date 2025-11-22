7e71dd3e7bcc4dac8234ba45e3df5aa432ba3ad6
yujin37 <yj061628@gmail.com>
2025-11-23 02:48:23 +0900

    feat(frontend): 타자 기록 컬럼 정렬 기능 추가

--- frontend/statsball/src/pages/player-stats/batters.tsx
+++ frontend/statsball/src/pages/player-stats/batters.tsx
@@ -26,46 +26,75 @@ interface Batter {
 }
 
 const BatterStats = () => {
-    const [loading, setLoading] = useState(false);
-    const [batters, setBatters] = useState<Batter[]>([]);
-    
-    const fetchAllBatters = async () => {
-    try {
-        setLoading(true);
-        const res = await axios.get<Batter[]>("/api/player/allPlayers");
-        setBatters(res.data);
-        console.log("전체 선수 데이터:", res.data);
-        } catch (error) {
-        console.error("전체 선수 데이터 가져오기 오류", error);
-        } finally {
-        setLoading(false);
-        }
-    };
-    useEffect(() => {
-        fetchAllBatters();
-    }, []);
-    const handleSearch = async (keyword: string, criteria: string) => {
-        console.log("검색기준: ", criteria, "Batters 검색:", keyword);
-        if (!keyword) {
-            // 검색어 없으면 전체 선수 다시 불러오기
-            fetchAllBatters();
-            return;
-        }
-        const searchSetUrl = "/api/player/searchPlayers?";
-        const query = `type=${encodeURIComponent(criteria)}&keyword=${encodeURIComponent(keyword)}`;
-        try{
-            setLoading(true);
-            const res = await axios.get<Batter[]>(
-                searchSetUrl + query
-            );
-            setLoading(false);
-            console.log("API 응답:", res.data);  
-            setBatters(res.data);
-        } catch(error) {
-            console.error('데이터 가져오기 오류', error);
-            return []
-        }
-    };
+  const [loading, setLoading] = useState(false);
+  const [batters, setBatters] = useState<Batter[]>([]);
+  
+  const [sortConfig, setSortConfig] = useState<{key: string, direction: "asc" | "desc"}>({
+      key: "name",
+      direction: "asc"
+  })
+  
+  const fetchAllBatters = async () => {
+  try {
+      setLoading(true);
+      const res = await axios.get<Batter[]>("/api/player/allPlayers");
+      setBatters(res.data);
+      console.log("전체 선수 데이터:", res.data);
+      } catch (error) {
+      console.error("전체 선수 데이터 가져오기 오류", error);
+      } finally {
+      setLoading(false);
+      }
+  };
+  useEffect(() => {
+      fetchAllBatters();
+  }, []);
+  const handleSearch = async (keyword: string, criteria: string) => {
+  console.log("검색기준: ", criteria, "Batters 검색:", keyword);
+  if (!keyword) {
+    // 검색어 없으면 전체 선수 다시 불러오기
+    fetchAllBatters();
+    return;
+  }
+  const searchSetUrl = "/api/player/searchPlayers?";
+  const query = `type=${encodeURIComponent(criteria)}&keyword=${encodeURIComponent(keyword)}`;
+  try{
+      setLoading(true);
+      const res = await axios.get<Batter[]>(
+          searchSetUrl + query
+      );
+      setLoading(false);
+      console.log("API 응답:", res.data);  
+      setBatters(res.data);
+  } catch(error) {
+      console.error('데이터 가져오기 오류', error);
+      return []
+  }
+  };
+  const sortedBatters = [...batters].sort((a, b) => {
+  const {key, direction} = sortConfig;
+  const order = direction === "asc" ? 1: -1;
+
+  const aVal = a[key as keyof Batter];
+  const bVal = b[key as keyof Batter];
+
+  if(typeof aVal === "number" && typeof bVal === "number") {
+      return (aVal - bVal) * order;
+  }
+  return aVal.toString().localeCompare(bVal.toString()) * order;
+});
+
+const handleSort = (key: string) => {
+  setSortConfig(prev => ({
+      key,
+      direction: prev.key === key && prev.direction === "asc" ? "desc":"asc"
+  }));
+}
+
+const renderIcon = (key: string) => {
+  if(!sortConfig || sortConfig.key !== key) return "↕"; 
+  return sortConfig.direction === "asc"?  "▲" : "▼";
+}
 
     return (
         <Layout>
@@ -85,30 +114,27 @@ const BatterStats = () => {
         <table>
           <thead>
             <tr>
-              <th>팀</th>
-              <th>이름</th>
-              <th>포지션</th>
-              <th>타율</th>
-              <th>출장 경기 수</th>
-              <th>타수</th>
-              <th>안타</th>
-              <th>홈런</th>
-              <th>득점</th>
-              <th>타점</th>
-              <th>도루</th>
-              <th>도루실패</th>
-              <th>삼진</th>
-              <th>4사구</th>
-              <th>출루율</th>
-              <th>OPS</th>
-              <th>득점권 타율</th>
+              <th onClick={() => handleSort("teams")}>팀{renderIcon("teams")}</th>
+              <th onClick={() => handleSort("name")}>이름{renderIcon("name")}</th>
+              <th onClick={() => handleSort("position")}>포지션{renderIcon("position")}</th>
+              <th onClick={() => handleSort("avg")}>타율{renderIcon("avg")}</th>
+              <th onClick={() => handleSort("game")}>출장 경기 수{renderIcon("game")}</th>
+              <th onClick={() => handleSort("atbats")}>타수{renderIcon("atbats")}</th>
+              <th onClick={() => handleSort("hits")}>안타{renderIcon("hits")}</th>
+              <th onClick={() => handleSort("home_runs")}>홈런{renderIcon("home_runs")}</th>
+              <th onClick={() => handleSort("runs_scored")}>득점{renderIcon("runs_scored")}</th>
+              <th onClick={() => handleSort("runs_batted_in")}>타점{renderIcon("runs_batted_in")}</th>
+              <th onClick={() => handleSort("stolen_bases")}>도루{renderIcon("stolen_bases")}</th>
+              <th onClick={() => handleSort("caught_stealing")}>도루실패{renderIcon("caught_stealing")}</th>
+              <th onClick={() => handleSort("strikeouts")}>삼진{renderIcon("strikeouts")}</th>
+              <th onClick={() => handleSort("wals")}>4사구{renderIcon("walks")}</th>
+              <th onClick={() => handleSort("on_base_percent")}>출루율{renderIcon("on_base_percent")}</th>
+              <th onClick={() => handleSort("ops")}>OPS{renderIcon("ops")}</th>
+              <th onClick={() => handleSort("avg_with_runners")}>득점권 타율{renderIcon("avg_with_runners")}</th>
             </tr>
           </thead>
           <tbody>
-            {batters
-              .slice()
-              .sort((a, b) => a.name.localeCompare(b.name))
-              .map((p) => (
+            {sortedBatters.map((p) => (
                 <tr key={p.id}>
                   <td>{p.teams}</td>
                   <td>{p.name}</td>
