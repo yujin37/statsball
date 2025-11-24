4eeff0508f97e0d34d678398293d26ad56514c9c
yujin37 <yj061628@gmail.com>
2025-11-24 11:20:53 +0900

    feat(frontend): 선수 페이지 내 투수, 타자 페이지 연결 추가

--- frontend/statsball/src/pages/players/playersAll.tsx
+++ frontend/statsball/src/pages/players/playersAll.tsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom"
 import Layout from "../../components/Layout/Layout"
-
+import styles from "./playersAll.module.scss"
 const PlayerInfo = () => {
     return (
         <Layout>
             <h2>전체 선수 정보</h2>
+            <ul>
+                <li>새로 업데이트 된 투수</li>
+            </ul>
+            <Link to = "/playerInfo/pitcherInfo" className={styles.navLink}> 투수 전체보기 </Link>
+            <ul>
+                <li> 새로 업데이트 된 타자 </li>
+            </ul>
+            <Link to = "/playerInfo/batterInfo" className={styles.navLink}> 타자 전체 보기</Link>
         </Layout>
     )
 }
