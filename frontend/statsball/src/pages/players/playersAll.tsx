472480fc36eb8e47a5a7d2cbcc110827ac5bfa9d
yujin37 <yj061628@gmail.com>
2025-11-24 13:12:32 +0900

    style(frontend): 전체 선수 정보 페이지 제목 및 링크 디자인 변경

--- frontend/statsball/src/pages/players/playersAll.tsx
+++ frontend/statsball/src/pages/players/playersAll.tsx
@@ -5,14 +5,14 @@ const PlayerInfo = () => {
     return (
         <Layout>
             <h2>전체 선수 정보</h2>
-            <ul>
-                <li>새로 업데이트 된 투수</li>
-            </ul>
+            <h3> 새로 업데이트 된 투수 </h3>
+            <div className={styles.navContainer}>
             <Link to = "/playerInfo/pitcherInfo" className={styles.navLink}> 투수 전체보기 </Link>
-            <ul>
-                <li> 새로 업데이트 된 타자 </li>
-            </ul>
+            </div>
+            <h3> 새로 업데이트 된 타자</h3>
+            <div className={styles.navContainer}>
             <Link to = "/playerInfo/batterInfo" className={styles.navLink}> 타자 전체 보기</Link>
+            </div>
         </Layout>
     )
 }
