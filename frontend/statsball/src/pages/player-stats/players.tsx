50b4439c6a1ee307b7b807d653ab916b1e7a9790
yujin37 <yj061628@gmail.com>
2025-11-24 12:54:26 +0900

    fix(frontend): 투수, 타자 순위 링크 수정 및 순위 표 추가

--- frontend/statsball/src/pages/player-stats/players.tsx
+++ frontend/statsball/src/pages/player-stats/players.tsx
@@ -23,55 +23,74 @@ const PlayerStats = () => {
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        const fetchTopPitcher = async() => {
+        const fetchTopWinPitcher = async() => {
             try{
                 setLoading(true)
                 const res = await axios.get<Pitcher[]>(
-                    "/api/pitcher/topWinPitchers"
+                    "/api/Ranking/pitcher/topPitchers?type=wins"
                 );
                 setTopPitchers(res.data);
+                console.log(res.data);
             } catch(error) {
                 console.error(`데이터 가져오기 오류`, error);
                 return []
             }
         }
-        const fetchTopBatter = async() => {
+        const fetchTopAvgBatter = async() => {
             try{
                 setLoading(true)
-                const res = await axios.get<Pitcher[]>(
-                    "/api/pitcher/topAvgBatters"
+                const res = await axios.get<Batter[]>(
+                    "/api/Ranking/player/topPlayers?type=avg"
                 );
-                setTopPitchers(res.data);
+                setTopBatters(res.data);
             } catch(error) {
                 console.error(`데이터 가져오기 오류`, error);
                 return []
             }
         }
-    })
+        fetchTopWinPitcher();
+        fetchTopAvgBatter();
+    }, [])
     return (
         <Layout>
             <h2>선수 기록</h2>
                 <h3>투수 최다 승 TOP3</h3>
+                <div className={styles.navContainer}>
+                    <Link to="/playerStats/pitchersStats" className={styles.navLink}>투수 기록 전체보기</Link>
+                </div>
+                <li className="header">
+                    <span>순위</span>
+                    <span>팀</span>
+                    <span>선수명</span>
+                    <span>승</span>
+                </li>
                 {topPitchers.map((p, idx) => (
-                    <li>
+                    <li key={p.id}>
                         <span>{idx+1}</span>
                         <span>{p.teams}</span>
                         <span>{p.name}</span>
                         <span>{p.wins}</span>
                     </li>
                 ))}
-                <Link to="/playerStats/pitchersStats" className={styles.navLink}>투수 기록</Link>
-                
                 <h3> 타자 타율 TOP3</h3>
+                <div className={styles.navContainer}>
+                    <Link to="/playerStats/battersStats" className={styles.navLink}>타자 기록 전체보기</Link>
+                </div>
+                <li className="header">
+                    <span>순위</span>
+                    <span>팀</span>
+                    <span>선수명</span>
+                    <span>타율</span>
+                </li>
                 {topBatters.map((b, idx) => (
-                    <li>
+                    <li key={b.id}>
                         <span>{idx+1}</span>
                         <span>{b.teams}</span>
                         <span>{b.name}</span>
                         <span>{b.avg}</span>
                     </li>
                 ))}
-                <Link to="/playerStats/battersStats" className={styles.navLink}>타자 기록</Link>
+                
         </Layout>
     )
 }
